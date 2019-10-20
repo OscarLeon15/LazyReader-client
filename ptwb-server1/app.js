76febc19f166224ce4c0f0e558f5272b79034b16
oscar leon <oscar.leon.delmar@gmail.com>
2019-10-20 14:08:06 -0500

    deleted a file

--- ptwb-server1/app.js
+++ ptwb-server1/app.js
@@ -52,7 +52,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 app.locals.title = 'Express - Generated with IronGenerator';
 
 
-
+//routes
 const index = require('./routes/index');
 app.use('/', index);
 
