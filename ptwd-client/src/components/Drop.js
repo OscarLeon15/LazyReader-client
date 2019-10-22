bff0d19c45289d012148e3a4f0b47c838a9f376d
Dusty Garon <dustygaron@hotmail.com>
2019-10-22 13:48:13 -0400

    working on the dropzone

--- ptwd-client/src/components/Drop.js
+++ ptwd-client/src/components/Drop.js
@@ -8,19 +8,32 @@ export default class Drop extends React.Component {
   }
 
   render() {
+    const maxSize = 1048576;
     return (
       <div className="text-center mt-5">
-        <Dropzone onDrop={this.onDrop}
+        <Dropzone
           onDrop={this.onDrop}
-          accept="image/png, image/gif">
-          {({ getRootProps, getInputProps, isDragActive }) => (
-            <div {...getRootProps()}>
-              <input {...getInputProps()} />
-              {!isDragActive && 'Click here or drop a file to upload!'}
-              {isDragActive && !isDragReject && "Drop it like it's hot!"}
-              {isDragReject && "File type not accepted, sorry!"}
-            </div>
-          )}
+          accept="image/png"
+          minSize={0}
+          maxSize={maxSize}
+        >
+          {({ getRootProps, getInputProps, isDragActive, isDragReject, rejectedFiles }) => {
+            const isFileTooLarge = rejectedFiles.length > 0 && rejectedFiles[0].size > maxSize;
+            return (
+              <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                {!isDragActive && 'Click here or drop a file to upload!'}
+                {isDragActive && !isDragReject && "Drop it like it's hot!"}
+                {isDragReject && "File type not accepted, sorry!"}
+                {isFileTooLarge && (
+                  <div className="text-danger mt-2">
+                    File is too large.
+                </div>
+                )}
+              </div>
+            )
+          }
+          }
         </Dropzone>
       </div>
     );
