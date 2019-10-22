de96f5a14ef2d858cc7c0eaf1bb8bd51511072ca
Dusty Garon <dustygaron@hotmail.com>
2019-10-22 13:30:05 -0400

    working on dropzone

--- ptwb-server1/app.js
+++ ptwb-server1/app.js
@@ -1,29 +1,29 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
-const session      = require("express-session");
-const cors         = require("cors"); 
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const session = require("express-session");
+const cors = require("cors");
 // const ibm          = require('ibm-watson');
 
-const user         = require('./models/User');
+const user = require('./models/User');
 
-mongoose
-  .connect('mongodb://localhost/ptwb-server1', {useNewUrlParser: true})
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err)
-  });
+// mongoose
+//   .connect('mongodb://localhost/ptwb-server1', {useNewUrlParser: true})
+//   .then(x => {
+//     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+//   })
+//   .catch(err => {
+//     console.error('Error connecting to mongo', err)
+//   });
 
-  // enables database connection
+// enables database connection
 require("./configs/database/db.setup");
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -39,11 +39,11 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
