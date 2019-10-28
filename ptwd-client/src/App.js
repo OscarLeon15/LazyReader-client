ad760c665df74aca4c830c64d3ae4500074f3e8f
Dusty Garon <dustygaron@hotmail.com>
2019-10-28 19:18:51 -0400

    cleaned up some code

--- ptwd-client/src/App.js
+++ ptwd-client/src/App.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import './App.css';
-// import axios from "axios";
-// import { Switch, Route, NavLink } from "react-router-dom";
-// import Signup from "./components/user-pages/Signup";
-// import CountriesList from "./components/CountriesList";
-// import Home from "./components/Home";
-// import Login from './components/user-pages/Login';
-
-// import MyUploader from "./components/MyUploader"
+import axios from "axios";
 import Nav from "./components/Nav"
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 
@@ -15,24 +8,18 @@ import 'react-bulma-components/dist/react-bulma-components.min.css';
 
 export default class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null
-    }
+
+  componentDidMount() {
+
+    console.log(process.env.REACT_APP_OCR)
+    axios.get("http://localhost:3000/api/checkuser", { withCredentials: true })
+      .then(responseFromTheBackend => {
+        console.log("User in APP.JS: ", responseFromTheBackend)
+        const { userDoc } = responseFromTheBackend.data;
+        this.syncCurrentUSer(userDoc);
+      })
+      .catch(err => console.log("Err while getting the user from the checkuser route: ", err))
   }
-
-  // componentDidMount() {
-
-  //   console.log(process.env.REACT_APP_OCR)
-  //   axios.get("http://localhost:3000/api/checkuser", { withCredentials: true })
-  //     .then(responseFromTheBackend => {
-  //       console.log("User in APP.JS: ", responseFromTheBackend)
-  //       const { userDoc } = responseFromTheBackend.data;
-  //       this.syncCurrentUSer(userDoc);
-  //     })
-  //     .catch(err => console.log("Err while getting the user from the checkuser route: ", err))
-  // }
 
   syncCurrentUSer(user) {
     this.setState({ currentUser: user })
--- ptwd-client/src/components/Nav.js
+++ ptwd-client/src/components/Nav.js
@@ -8,52 +8,66 @@ import Signup from "../components/user-pages/Signup"
 import Login from '../components/user-pages/Login'
 
 
-export default () => (
-  <div>
+export default class Nav extends React.Component {
 
-    <nav className="navbar is-light" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <NavLink to="/" className="navbar-item" >
-          <img src={logo} width="152" height="28" alt="lazy reader logo" />
-        </NavLink>
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
 
-        <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-        </a>
-      </div>
+  render() {
+    return (
+      <div>
 
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-        </div>
+        <nav className="navbar is-light" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <NavLink to="/" className="navbar-item" >
+              <img src={logo} width="152" height="28" alt="lazy reader logo" />
+            </NavLink>
 
-        <div className="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <NavLink to="/signup-page" class="button is-primary">
-                <strong>Sign up</strong>
-              </NavLink>
-              <NavLink to="/login-page" class="button is-light">
-                Log in
+            <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" className="navbar-menu">
+            <div className="navbar-start">
+            </div>
+
+            <div className="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                  <NavLink to="/signup-page" class="button is-primary">
+                    <strong>Sign up</strong>
+                  </NavLink>
+                  <NavLink to="/login-page" class="button is-light">
+                    Log in
           </NavLink>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signup-page" render={() =>
+            <Signup
+              currentUser={this.state.currentUser}
+              onUserChange={userDoc => this.syncCurrentUSer(userDoc)}
+            />
+          } />
+          {/* Login component */}
+          <Route exact path="/login-page" component={Login} />
+        </Switch>
+
       </div>
-    </nav>
 
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/signup-page" render={() =>
-        <Signup
-          currentUser={this.state.currentUser}
-          onUserChange={userDoc => this.syncCurrentUSer(userDoc)}
-        />
-      } />
-      {/* Login component */}
-      <Route exact path="/login-page" component={Login} />
-    </Switch>
 
-  </div>
-)
+    )
+  }
+}
