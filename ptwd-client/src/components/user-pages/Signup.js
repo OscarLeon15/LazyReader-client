c9cae702113a60b5590add849b49bcb1a095281b
Dusty <dustygaron@hotmail.com>
2019-11-04 18:16:45 -0500

    OCR working and fixed some login issues

--- ptwd-client/src/components/user-pages/Signup.js
+++ ptwd-client/src/components/user-pages/Signup.js
@@ -3,91 +3,133 @@ import axios from "axios";
 
 
 export default class Signup extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             fullName: "",
             email: "",
-            password:"",
+            password: "",
             message: null
         }
     }
 
-    genericSync(event){
-        // console.log("what is: ", event.target.value)
+    genericSync(event) {
+        console.log("what is: ", event.target.value)
         const { name, value } = event.target;
         this.setState({ [name]: value });
     }
 
-    handleSubmit (event){
-        // console.log("submitting form");
+    handleSubmit(event) {
+        console.log("=====>" + this.state.fullName, this.state.email, this.state.password, this.state.message);
+        console.log("submitting form");
         event.preventDefault();
 
         axios.post(
             // route we are hitting in the backend
-            "http://localhost:3000/api/signup",
+            "http://localhost:5000/api/signup",
             // the data from the form (AKA req.body 🚀) that we are sending to this route to do the job
             this.state,
             // secure sending
             { withCredentials: true }
         )
-        .then( responseFromServer => {
-            // console.log("response is:", responseFromServer);
-            const { userDoc } = responseFromServer.data;
-            this.props.onUserChange(userDoc);
-        })
-        .catch( err => console.log("Err in signup: ", err));
+            .then(responseFromServer => {
+                console.log("response is:", responseFromServer);
+                const { userDoc } = responseFromServer.data;
+                this.props.onUserChange(userDoc);
+            })
+            .catch(err => console.log("Err in signup: ", err));
     }
 
-    render(){
+    render() {
         console.log("Do I have user in Signup: ", this.props.currentUser)
         const { fullName, email, password } = this.state;
-        // console.log("STATE: ", this.state);
-        if(this.props.currentUser){
-            return(
+        console.log("STATE: ", this.state);
+        if (this.props.currentUser) {
+            return (
                 <div>
-                    <h2> Welcome to your app, { this.props.currentUser.fullName } ! You're signed in! </h2>
+                    <h2> Welcome to your app, {this.props.currentUser.fullName} ! You're signed in! </h2>
                 </div>
             )
         }
 
         return (
-            <section>
-                <h2> Sign up </h2>
-                <form onSubmit ={ event => this.handleSubmit(event) } >
-                    <label> Full name: </label>
-                    <input
-                        value={fullName} // this.state.fullName
-                        onChange = { event => this.genericSync(event) } 
-                        type="text"
-                        name="fullName"
-                        placeholder="yo"
-                    />
-
-                     <label> Email: </label>
-                    <input
-                        value={email} // this.state.email
-                        onChange = { event => this.genericSync(event) } 
-                        type="email"
-                        name="email"
-                        placeholder="my-email@ironhack.com"
-                    />
-
-                    <label> Password</label>
-                    <input
-                        value={password} // this.state.password
-                        onChange = { event => this.genericSync(event) } 
-                        type="password"
-                        name="password"
-                        placeholder="***********"
-                    />
-                    <button> Sign Up </button>
-                </form>
-                {/* if the message is not null (basically if there's a message) then show it in this <div> tag */}
-                { this.state.message && <div> { this.state.message } </div> }
-            </section>
+            <div className="columns is-centered">
+                <div className="column is-5-tablet is-4-desktop is-3-widescreen">
+                    <h1> Sign Up </h1>
 
 
+                    <form onSubmit={event => this.handleSubmit(event)} >
+                        {/*----- FULL NAME -----*/}
+                        <div className="field">
+                            <p className="control has-icons-left has-icons-right">
+                                <input
+                                    className="input"
+                                    value={fullName} // this.state.email
+                                    onChange={event => this.genericSync(event)}
+                                    type='text'
+                                    name='fullName'
+                                    placeholder='Full Name'
+                                />
+                                <span className="icon is-small is-left">
+                                    <i className="fa fa-envelope"></i>
+                                </span>
+                                <span className="icon is-small is-right">
+                                    <i className="fa fa-check"></i>
+                                </span>
+                            </p>
+                        </div>
+
+
+                        {/*----- EMAIL -----*/}
+                        <div className="field">
+                            <p className="control has-icons-left has-icons-right">
+                                <input
+                                    className="input"
+                                    value={email} // this.state.email
+                                    onChange={event => this.genericSync(event)}
+                                    type='email'
+                                    name='email'
+                                    placeholder='myname@email.com'
+                                />
+                                <span className="icon is-small is-left">
+                                    <i className="fa fa-envelope"></i>
+                                </span>
+                                <span className="icon is-small is-right">
+                                    <i className="fa fa-check"></i>
+                                </span>
+                            </p>
+                        </div>
+
+                        {/*----- PASSWORD -----*/}
+                        <div className="field">
+                            <p className="control has-icons-left">
+                                <input
+                                    className="input"
+                                    value={password} // this.state.password
+                                    onChange={event => this.genericSync(event)}
+                                    type='password'
+                                    name='password'
+                                    placeholder='***********'
+                                />
+                                <span className="icon is-small is-left">
+                                    <i className="fa fa-lock"></i>
+                                </span>
+                            </p>
+                        </div>
+
+                        {/*----- SUBMIT -----*/}
+                        <div className="field">
+                            <p className="control">
+                                <button className="button is-success">
+                                    Sign Up
+                                </button>
+                            </p>
+                        </div>
+
+                    </form>
+                    {this.state.message && <div> {this.state.message} </div>}
+                </div>
+            </div>
 
 
 
