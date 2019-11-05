ce52d098b16ac0b1c8516ee18b3ddf3aa6b92d2b
Dusty <dustygaron@hotmail.com>
2019-11-05 14:52:27 -0500

    troubleshooting

--- ptwd-client/src/components/MyUploader.js
+++ ptwd-client/src/components/MyUploader.js
@@ -3,9 +3,7 @@ import React from 'react'
 import 'react-dropzone-uploader/dist/styles.css'
 import Dropzone from 'react-dropzone-uploader'
 import axios from 'axios'
-// import file from '../img/file.svg'
-import ocrSpaceApi from 'ocr-space-api';
-require('dotenv').config()
+// import Watson from '../components/Watson'
 
 
 export default class MyUploader extends React.Component {
@@ -15,8 +13,8 @@ export default class MyUploader extends React.Component {
     this.state = {
       file: null
     }
+    // this.handleSubmit = this.handleSubmit.bind(this)
   }
-
 
   // specify upload params and url for your files
   getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
@@ -34,77 +32,64 @@ export default class MyUploader extends React.Component {
     console.log(files.map(f => f.meta))
     allFiles.forEach(f => f.remove())
 
-    // Start Cloudinary -----------------------
+    // Axios Call -----------------------
     let formData = new FormData();
     formData.append('theImage', this.state.file)
+    // console.log("THIS STATE FILE=====>>>" + this.state.file)
 
     axios.post('http://localhost:5000/testing', formData)
       .then(responseFromTheBackend => {
-        // responseFromTheBackend.data is the image url
-        console.log(responseFromTheBackend.data)
-
-        // let options = {
-        //   apikey: process.env.REACT_APP_OCR,
-        //   language: 'eng',
-        //   imageFormat: 'image/png', // Image Type (Only png ou gif is acceptable at the moment i wrote this)
-        //   isOverlayRequired: true
-        // };
-
-        // Image file to upload
-        // let imageFilePath = responseFromTheBackend.data;
-
-        // Run and wait the result
-        // ocrSpaceApi.parseImageFromLocalFile(imageFilePath, options)
-        //   .then((parsedResults) => {
-        //     console.log(parsedResults)
-        //     console.log('parsedText: \n', parsedResults.parsedText);
-        //     console.log('ocrParsedResults: \n', parsedResults.ocrParsedResult);
-        //   }).catch((err) => {
-        //     console.log('ERROR:', err);
-        //   });
-
+        // console.log(responseFromTheBackend.data)
         // Example API Request -----------------------------
-        var https = require('https');
+        let https = require('https');
 
-        var options = {
+        let options = {
           'method': 'GET',
           'hostname': 'api.ocr.space',
-          'path': `/parse/imageurl?apikey=86be69917788957&url=${responseFromTheBackend.data}&language=chs&isOverlayRequired=true`,
+          'path': `/parse/imageurl?apikey=86be69917788957&url=${responseFromTheBackend.data}&isOverlayRequired=false`,
           'headers': {
             'apikey': '86be69917788957'
           }
         };
 
-        var req = https.request(options, function (res) {
-          var chunks = [];
+        let req = https.request(options, function (res) {
+          let chunks = [];
 
           res.on("data", function (chunk) {
             chunks.push(chunk);
           });
 
           res.on("end", function (chunk) {
-            var body = Buffer.concat(chunks);
-            console.log(body.toString());
+            let body = Buffer.concat(chunks);
+            let stringBody = body.toString()
+            let newString = JSON.stringify(stringBody)
+            console.log(newString)
           });
 
           res.on("error", function (error) {
             console.error(error);
           });
+
         });
 
-        var postData = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"language\"\r\n\r\neng\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"isOverlayRequired\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"url\"\r\n\r\nhttp://dl.a9t9.com/ocrbenchmark/eng.png\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"iscreatesearchablepdf\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"issearchablepdfhidetextlayer\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--";
+
+        var postData = "------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data;name=\"language\"\r\n\r\neng\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"isOverlayRequired\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"url\"\r\n\r\nhttp://dl.a9t9.com/ocrbenchmark/eng.png\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"iscreatesearchablepdf\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"issearchablepdfhidetextlayer\"\r\n\r\nfalse\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--";
 
         req.setHeader('content-type', 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW');
 
         req.write(postData);
 
         req.end();
-        // -----------------------------  
 
+        // let newString = JSON.stringify(req)
+        // console.log("THIS IS JSON STRINGIFY REQ ======>" + JSON.stringify(req))
+        // -----------------------------  
       })
 
       .catch(err => console.log("error from inside Form Data", err))
+
   }
+
 
   render() {
     return (
@@ -117,11 +102,16 @@ export default class MyUploader extends React.Component {
           onSubmit={this.handleSubmit}
           accept=".png, .jpg, .jpeg, .pdf"
         />
+        {/* <Watson name={this.newString} /> */}
       </div>
     )
   }
+  // --------------------------------------------------------
+
+
+
+
+
+
 
 }
-
-{/* <Image publicId="file-converter-project-3/filename.filetype" >
-</Image> */}
